ff593f4a1a97a40ea815078d91f601c53cab95d2
邓晓桢 <xiaozhenyinhu@gmail.com>
2023-09-26 16:48:34 +0800

    feat: 保存二维码

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,26 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { randomUUID } from 'crypto';
 import * as qrcode from 'qrcode';
+const map = new Map<string, QrCodeInfo>();
+
+interface QrCodeInfo {
+  /** 扫码状态
+   * - noscan：未扫描
+   * - scan-wait-confirm：已扫描，等待用户确认
+   * - scan-confirm：已扫描，用户同意授权
+   * - scan-cancel：已扫描，用户取消授权
+   * - expired：已过期
+   */
+  status:
+    | 'noscan'
+    | 'scan-wait-confirm'
+    | 'scan-confirm'
+    | 'scan-cancel'
+    | 'expired';
+  userInfo?: {
+    userId: number;
+  };
+}
 
 @Controller()
 export class AppController {
@@ -15,7 +35,12 @@ export class AppController {
   @Get('qrcode/generate')
   async generate() {
     const uuid = randomUUID();
-    const dataUrl = await qrcode.toDataURL(uuid);
+    const dataUrl = await qrcode.toDataURL(
+      `http://localhost:3000/pages/confirm.html?id=${uuid}`,
+    );
+    map.set(`qrcode_${uuid}`, {
+      status: 'noscan',
+    });
     return {
       qrcode_id: uuid,
       img: dataUrl,
