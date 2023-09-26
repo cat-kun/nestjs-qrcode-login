437badd9bad44aa422a6b797f3187baedb3c2a32
邓晓桢 <xiaozhenyinhu@gmail.com>
2023-09-26 16:16:55 +0800

    feat: 生成二维码

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { randomUUID } from 'crypto';
+import * as qrcode from 'qrcode';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('qrcode/generate')
+  async generate() {
+    const uuid = randomUUID();
+    const dataUrl = await qrcode.toDataURL(uuid);
+    return {
+      qrcode_id: uuid,
+      img: dataUrl,
+    };
   }
 }
